0d1f905c2e2f1cf42184705ec5ef3c0f5b998f35
E1sad <106135426+E1sad@users.noreply.github.com>
2022-08-29 18:54:37 +0400

    Simple Input and Movement

---
+++ GunsForSurvival/Assets/App/Scripts/Characters/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SOG.Characters.Player
+{
+  public class PlayerMovement : MonoBehaviour
+  {
+    [Header("Link refenrece")]
+    [SerializeField] private Rigidbody2D playerRb;
+
+    [Header("Parameters")]
+    [SerializeField] private float speed;
+
+    private void Movement()
+    {
+      float horizontalInput = SimpleInput.GetAxis("Horizontal");
+      float verticalInput = SimpleInput.GetAxis("Vertical");
+
+      playerRb.velocity = ((transform.up * verticalInput * speed) + (transform.right * horizontalInput * speed))*Time.deltaTime;
+
+
+    }
+
+
+    private void Update()
+    {
+      Movement();
+    }
+  }
+}
