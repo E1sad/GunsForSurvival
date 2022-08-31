ea1c12e26a4933fdc4bb0946fde07d26bbe0fb31
E1sad <106135426+E1sad@users.noreply.github.com>
2022-08-31 21:44:59 +0400

    full animation with movement

--- GunsForSurvival/Assets/App/Scripts/Characters/Player/PlayerMovement.cs
+++ GunsForSurvival/Assets/App/Scripts/Characters/Player/PlayerMovement.cs
@@ -19,9 +19,33 @@ namespace SOG.Characters.Player
       float horizontalInput = SimpleInput.GetAxis("Horizontal");
       float verticalInput = SimpleInput.GetAxis("Vertical");
 
+      playerAnimator.SetFloat("Horizontal", horizontalInput);
+      playerAnimator.SetFloat("Vertical", verticalInput);
+      playerRb.velocity = ((transform.up * verticalInput * speed) + (transform.right * horizontalInput * speed))*Time.deltaTime;
       
 
-      playerRb.velocity = ((transform.up * verticalInput * speed) + (transform.right * horizontalInput * speed))*Time.deltaTime;
+
+      if (0.1<Mathf.Abs(horizontalInput) || 0.1 < Mathf.Abs(verticalInput))
+      {
+        playerAnimator.SetBool("IsMove", true);
+
+        if(horizontalInput>0)
+        {
+          gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(horizontalInput<0)
+        {
+          gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+      }
+      else
+      {
+        playerAnimator.SetBool("IsMove", false);
+      }
+      
+
+    
     }
 
 
